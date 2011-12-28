39a98556065c1021fbf36bad37ff28a6f4d0d6f6
Roman <roman@burningswell.com>
2011-12-28 22:27:29 +0100

    Use Leiningen 1.6.2 in dev dependencies.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject lein-env "0.0.1-SNAPSHOT"
   :description "Leiningen project environments."
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[leiningen/leiningen "1.6.3-SNAPSHOT"]])
+  :dev-dependencies [[leiningen/leiningen "1.6.2"]])
