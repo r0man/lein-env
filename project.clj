e34584e0b6a48f4711a9a04d830535b3635032f9
Roman <roman@burningswell.com>
2011-12-28 22:07:50 +0100

    Changed project description.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject lein-env "1.0.0-SNAPSHOT"
-  :description "Leiningen environments."
+  :description "Leiningen project environments."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[leiningen/leiningen "1.6.3-SNAPSHOT"]])
