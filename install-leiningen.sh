9c101ad535169485c531be69b64089a1130c11f7
Roman <roman@burningswell.com>
2012-02-05 19:54:13 +0100

    Changed download url.

--- install-leiningen.sh
+++ install-leiningen.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl https://github.com/downloads/technomancy/leiningen/leiningen-1.6.2-standalone.jar > /tmp/leiningen-1.6.2.jar
+curl http://cloud.github.com/downloads/technomancy/leiningen/leiningen-1.6.2-standalone.jar > /tmp/leiningen-1.6.2.jar
 mvn install:install-file -DgroupId=leiningen -DartifactId=leiningen -Dversion=1.6.2 -Dpackaging=jar -Dfile=/tmp/leiningen-1.6.2.jar
