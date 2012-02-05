8728c2a0c1830b8cfedd4fd06382b939dc25d0b9
Roman <roman@burningswell.com>
2012-02-05 20:01:52 +0100

    Added clojure to dev dependencies.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [org.clojure/tools.logging "0.2.3"]
                  [robert/hooke "1.1.2"]]
   :dev-dependencies [[leiningen/leiningen "1.6.2"]
+                     [org.clojure/clojure "1.3.0"]
                      [org.clojure/tools.logging "0.2.3"]
                      [org.slf4j/slf4j-log4j12 "1.5.6"]]
   :environments {:development {:api-key "LEIN-PROJECT-DEVELOPMENT-API-KEY"}
