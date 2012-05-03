88227f4eb8218b2dd5262d0ff8df0ea58c49504a
Roman <roman@burningswell.com>
2012-05-03 22:48:22 +0200

    Switched to Clojure 1.4.0.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject lein-env/lein-env "0.0.3-SNAPSHOT"
   :description "Leiningen project environments."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [robert/hooke "1.1.2"]]
   :min-lein-version "2.0.0"
-  :profiles {:dev {:dependencies [[leiningen/leiningen "2.0.0-preview2"]]
-                   :resource-paths ["test-resources"]}}
+  :profiles {:dev {:resource-paths ["test-resources"]}}
+  :eval-in :leiningen
   :environments {:development
                  {:api-key "LEIN-PROJECT-DEVELOPMENT-API-KEY"},
                  :test {:api-key "LEIN-PROJECT-TEST-API-KEY"}})
